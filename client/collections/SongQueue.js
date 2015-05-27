1d889b335efbc84548da576fc00ee3c81088356b
Hack Reactor Students <accounts@hackreactor.com>
2015-05-27 14:48:43 -0700

    play when only one song in SQ, can remove songs from SQ

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,19 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    this.on('add', function(song){
+      if(this.length === 1){
+        this.playFirst();
+      }
+    }, this);
+    this.on('ended', function() {
+      this.remove(this.at(0));
+    }, this)
+  },
+
+  playFirst: function(){
+
   }
+
 
 });
